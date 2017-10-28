9e0c96703641cf13760214bfa30ec4fb0476fabd
David M <davidmh.mail@gmail.com>
2017-10-27 20:00:47 -0500

    Style taskTimer and breakTimer - working

--- app/scripts/controllers/TasksCtrl.js
+++ app/scripts/controllers/TasksCtrl.js
@@ -6,40 +6,38 @@
   function TasksCtrl(timer) {
     var vm = this;
 
-    vm.startTimer = startTimer;
-    vm.resetTimer = resetTimer;
-    vm.resetBreakTimer = resetBreakTimer;
+    // vm.startTimer = startTimer;
+    // vm.resetTimer = resetTimer;
+    // vm.resetBreakTimer = resetBreakTimer;
     vm.timerTypeIsTask = timerTypeIsTask;
     vm.getTimerOn = getTimerOn;
-    vm.getTaskTimer = getTaskTimer;
-
-    ///////////
-
-    function startTimer() {
-      timer.startTimer();
-    }
-
-    function resetTimer() {
-      timer.resetTimer();
-    }
-
-    function resetBreakTimer() {
-      timer.resetTimer(true);
-    }
-
+    // vm.getTaskTimer = getTaskTimer;
+    //
+    // ///////////
+    //
+    // function startTimer() {
+    //   timer.startTimer();
+    // }
+    //
+    // function resetTimer() {
+    //   timer.resetTimer();
+    // }
+    //
+    // function resetBreakTimer() {
+    //   timer.resetTimer(true);
+    // }
+    //
     function timerTypeIsTask() {
-      timer.timerTypeIsTask ? vm.resetButtonText = 'Reset' : vm.resetButtonText = 'End Break';
-
       return timer.timerTypeIsTask;
     }
 
     function getTimerOn() {
       return timer.timerOn;
     }
-
-    function getTaskTimer() {
-      return timer.taskTimer;
-    }
+    //
+    // function getTaskTimer() {
+    //   return timer.taskTimer;
+    // }
 
   }
 
